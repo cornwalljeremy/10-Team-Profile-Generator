906261ff10218cf723898275c4552bd70518e4d3
cornwalljeremy <cornwall.jeremy@gmail.com>
2020-10-02 20:36:24 -0600

    Communication

--- __Tests__/Employee.test.js
+++ __Tests__/Employee.test.js
@@ -1,9 +1,9 @@
-function Employee(name, id, email){
-    this.nameInput = name;
-    this.idInput = id;
-    this.emaiInput = email;
+// const  test = require("jest");
+const Employee = require("../lib/Employee");
 
-    this.createCard = function(){
-        if
-    }
-}
+test('Creates and Employee', () => {
+    const subject = new Employee('George', 911, 'george@george.com');
+    expect(subject.name).toEqual(expect.any(String));
+    expect(subject.id).toEqual(expect.any(Number));
+    expect(subject.email).toEqual(expect.any(String));
+})
--- index.js
+++ index.js
@@ -1,44 +1,63 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
+const Employee = require("./lib/Employee");
+let testEmployee;
 
-inquirer.prompt([
-  {
-    type: "input",
-    name: "name",
-    message: "Please enter team members name. ",
-    validate: (nameInput) => {
-      if (nameInput) {
-        return true;
-      } else {
-        console.log("Please Enter a name of a team member");
-        return false;
-      }
-    },
-  },
-  {
-    type: "input",
-    name: "id",
-    message: "Enter the employee ID",
-    validate: (idInput) => {
-      if (idInput) {
-        return true;
-      } else {
-        console.log("enter an ID");
-        return false;
-      }
-    },
-  },
-  {
-    type: "input",
-    name: "email",
-    message: "Enter the employee email",
-    validate: (emailInput) => {
-      if (emailInput) {
-        return true;
-      } else {
-        console.log("enter an email.");
-        return false;
-      }
-    },
-  },
-]);
+function startQuestions() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Please enter team members name. ",
+        validate: (nameInput) => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log("Please Enter a name of a team member");
+            return false;
+          }
+        },
+      },
+      // {
+      //   type: "input",
+      //   name: "id",
+      //   message: "Enter the employee ID",
+      //   validate: (idInput) => {
+      //     if (idInput) {
+      //       return true;
+      //     } else {
+      //       console.log("enter an ID");
+      //       return false;
+      //     }
+      //   },
+      // },
+      // {
+      //   type: "input",
+      //   name: "email",
+      //   message: "Enter the employee email",
+      //   validate: (emailInput) => {
+      //     if (emailInput) {
+      //       return true;
+      //     } else {
+      //       console.log("enter an email.");
+      //       return false;
+      //     }
+      //   },
+      // },
+    ])
+    .then(function (data) {
+      console.log(data);
+      testEmployee = new Employee(data.name, 12345, "jer@jer.com");
+      console.log(testEmployee);
+    });
+}
+
+// let name = new Employee();
+// let id = new Employee(`${id}`);
+// let email = new Employee(`${email}`);
+
+// name.getName()
+// id.getId()
+// email.getEmail();
+startQuestions();
